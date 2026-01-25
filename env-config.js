90e02222db115866b1c6f4df5cb694f3a2702a39
Luke <lukelangford858@gmail.com>
2026-01-24 17:52:26 -0800

    Fix: Commit env-config.js template so Netlify can publish it

---
+++ env-config.js
@@ -0,0 +1,6 @@
+// This file is auto-generated during build
+// DO NOT EDIT - it will be overwritten
+window.ENV = {
+  CLOUDINARY_CLOUD_NAME: 'PLACEHOLDER',
+  CLOUDINARY_UPLOAD_PRESET: 'PLACEHOLDER'
+};
